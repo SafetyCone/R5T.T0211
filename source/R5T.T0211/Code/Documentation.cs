2704ea38a2c293cc8d2e51689eb8c11e0f02c301
davidcoats <david@getsafetycone.com>
2023-06-30 10:47:08 -0700

    Initial commit.

---
+++ source/R5T.T0211/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0211
+{
+	/// <summary>
+	/// .NET documentation XML file strong types library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0211/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.T0211
+{
+    public static class Instances
+    {
+		
+    }
+}
